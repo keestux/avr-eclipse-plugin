8a954a569133cd9e9bab4eba7cd1d8d5dc9e14db
Thomas Holland <thomas@innot.de>
2009-10-10 15:47:08 +0000

    added test for BaseByteProperties
    svn path=/branches/R2.3 Maintenance/; revision=751

--- de.innot.avreclipse.core.test/tests/de/innot/avreclipse/TestHelper.java
+++ de.innot.avreclipse.core.test/tests/de/innot/avreclipse/TestHelper.java
@@ -16,8 +16,18 @@
 package de.innot.avreclipse;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
@@ -41,12 +51,14 @@ public class TestHelper {
 	 * @param filename
 	 *            Path and name of the file relative to the plugin root.
 	 * @return URL of the file.
+	 * @throws IOException
 	 */
-	public static URL getPluginFileURL(String filename) {
+	public static URL getPluginFileURL(String filename) throws IOException {
 		AVRPlugin plugin = AVRPlugin.getDefault();
 		Bundle bundle = plugin.getBundle();
 		URL url = FileLocator.find(bundle, new Path(filename), null);
-		return url;
+		URL fileurl = FileLocator.toFileURL(url);
+		return fileurl;
 	}
 
 	/**
@@ -58,10 +70,18 @@ public class TestHelper {
 	 * @param filename
 	 *            Path and name of the file relative to the plugin root.
 	 * @return <code>File</code>
+	 * @throws IOException
 	 */
-	public static File getPluginFile(String filename) {
+	public static File getPluginFile(String filename) throws IOException {
 		URL url = getPluginFileURL(filename);
-		return new File(url.getFile());
+		try {
+			return new File(url.toURI());
+		} catch (URISyntaxException e) {
+			// Unlikely to happen
+			e.printStackTrace();
+		}
+
+		return null;
 	}
 
 	/**
@@ -73,9 +93,58 @@ public class TestHelper {
 	 * @param filename
 	 *            Path and name of the file relative to the plugin root.
 	 * @return <code>IPath</code>
+	 * @throws IOException
 	 */
-	public static IPath getPluginFilePath(String filename) {
-		URL url = getPluginFileURL(filename);
-		return new Path(url.getFile());
+	public static IPath getPluginFilePath(String filename) throws IOException {
+		File file = getPluginFile(filename);
+		return new Path(file.getAbsolutePath());
+	}
+
+	/**
+	 * Get an <code>IFile</code> resource for a file in the test plugin.
+	 * <p>
+	 * Because <code>IFiles</code> can only exist within a Workspace this method
+	 * will create a new Project in the current Workspace and link the requested
+	 * file to the project.<br>
+	 * The name of the project will be equivalent to the path of the given file.
+	 * </p>
+	 * 
+	 * @param filepath
+	 *            Path of a file in the test plugin.
+	 * @return <code>IFile</code> to a link to the given file.
+	 * @throws IOException
+	 * @throws CoreException
+	 */
+	public static IFile getPluginIFile(String filepath) throws IOException,
+			CoreException {
+
+		IPath fullpath = new Path(filepath);
+		String filename = fullpath.lastSegment();
+		String projectname = fullpath.removeLastSegments(1).toString();
+		projectname = projectname.replace('/', '_'); // remove path separators
+
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceRoot root = workspace.getRoot();
+		IProject project = root.getProject(projectname);
+		IFile file = project.getFile(filename);
+
+		if (!project.exists())
+			project.create(null);
+		if (!project.isOpen())
+			project.open(null);
+
+		if (!file.exists()) {
+			try {
+				URI origuri = getPluginFileURL(filepath).toURI();
+				file.createLink(origuri, IResource.NONE, null);
+			} catch (URISyntaxException e1) {
+				// unlikely to happen. If it does happen wrap in a IOException
+				e1.printStackTrace();
+				throw new IOException(e1);
+			}
+		}
+
+		return file;
+
 	}
 }
