ee1444244a0feccd3516ae17e4835a55b2c975ff
Thomas Holland <thomas@innot.de>
2010-06-17 11:10:21 +0000

    Fixed one compilation error with Helios
    svn path=/branches/R2.3 Maintenance/; revision=773

--- de.innot.avreclipse.core/src/de/innot/avreclipse/mbs/scannerconfig/AVRGCCScannerInfoCollector.java
+++ de.innot.avreclipse.core/src/de/innot/avreclipse/mbs/scannerconfig/AVRGCCScannerInfoCollector.java
@@ -50,7 +50,7 @@ public class AVRGCCScannerInfoCollector extends PerProjectSICollector implements
 	 *      org.eclipse.cdt.make.core.scannerconfig.ScannerInfoTypes)
 	 */
 	@Override
-	public List<?> getCollectedScannerInfo(Object resource, ScannerInfoTypes type) {
+	public List<String> getCollectedScannerInfo(Object resource, ScannerInfoTypes type) {
 		// Check if the requested ScannerInfoType is TARGET_SPECIFIC_OPTION
 		// If no, let the superclass handle this.
 		// If yes, return the "-mmcu" compiler option with the project MCDU
